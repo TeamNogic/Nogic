64fdd910dc093e97852ab671dd77e5a99d1c6b53
ken60 <kenken60826@gmail.com>
2016-07-27 15:18:26 +0900

    fix

---
+++ Assets/Debug_Scene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Debug_Scene : MonoBehaviour
+{
+    private GUIStyle style = new GUIStyle();
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        style = new GUIStyle();
+        style.fontSize = 20;
+        style.normal.textColor = Color.red;
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(20, 20, 100, 50), "_AlphaValue: " + SceneManager.GetActiveScene().name, style);
+    }
+}
